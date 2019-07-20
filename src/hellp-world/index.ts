19aeaf75f594da6a84c16bbdb3b5fdf33cf6b49a
Bram Borggreve <borggreve@gmail.com>
2019-07-20 02:00:26 +0200

    fix: add console.log message

--- src/hellp-world/index.ts
+++ src/hellp-world/index.ts
@@ -5,6 +5,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 // per file.
 export function helloWorldSchematic(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    console.log('helloWorldSchematic')
     return tree;
   };
 }
